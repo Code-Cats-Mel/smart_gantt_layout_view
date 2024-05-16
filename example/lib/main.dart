50a9d4f13eec148dd9f70cd5bb0ae9504414886f
River Huang <rhuang@aglet.app>
2024-05-16 11:58:18 +1000

    Update line to GanttCard

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -68,8 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     (left: 0.7, length: 0.1),
                   ],
                   ganttCardBuilder: (index) {
-                    return const Line(
-                      // height: 10,
+                    return const GanttCard(
                       width: double.infinity,
                       color: Colors.red,
                     );
@@ -87,26 +86,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Line extends StatelessWidget {
+class GanttCard extends StatelessWidget {
   final double? height;
   final double width;
   final Color color;
-  final double circularRadius;
 
-  const Line({super.key, this.height, required this.width, required this.color})
-      : circularRadius =
-            (height != null && width > height) ? height / 2 : width / 2;
+  const GanttCard(
+      {super.key, this.height, required this.width, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    print('width: $width, height: $height, circularRadius: $circularRadius');
     return Container(
-      // height: height,
+      height: height,
       width: width,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.all(Radius.circular(circularRadius)),
-      ),
+      color: color,
     );
   }
 }
